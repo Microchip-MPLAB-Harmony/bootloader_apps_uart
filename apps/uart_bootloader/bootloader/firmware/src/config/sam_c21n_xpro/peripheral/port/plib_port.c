9ea6fae3649b55bc9929a79b8615346c280d8ee0
Amit Raddi <amit.raddi@microchip.com>
2020-07-07 11:13:42 +0530

    [UART] Regenerated demo for uart_bootloader_bootloader_sam_c21n_xpro
    MH3-37563
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_bootloader/bootloader/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
+++ apps/uart_bootloader/bootloader/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
@@ -73,6 +73,7 @@ void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
 
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000;
    PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x1;
@@ -81,8 +82,10 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x33;
    PORT_REGS->GROUP[1].PORT_PMUX[9] = 0x3;
+                
    /************************** GROUP 2 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
