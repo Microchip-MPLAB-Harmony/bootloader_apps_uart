d054192366e4892a598f461b33de6fa4581b1103
Amit Raddi <amit.raddi@microchip.com>
2020-07-07 11:18:06 +0530

    [UART] Regenerated demo for uart_bootloader_bootloader_sam_da1_xpro
    MH3-37571
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_bootloader/bootloader/firmware/src/packs/ATSAMDA1J16B_DFP/atsamda1j16b_compat.h
+++ apps/uart_bootloader/bootloader/firmware/src/packs/ATSAMDA1J16B_DFP/atsamda1j16b_compat.h
@@ -1,5 +1,26 @@
 /*******************************************************************************
-* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+  Device compatibility Header File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    atsamda1j16b_compat.h
+
+  Summary:
+    This file includes deprecated macro names that are retained for the purpose
+    of compatibility with Harmony 3 Middleware. Users are discouraged from using
+    macros defined in this file. Recommend to use macros defined in the device
+    header file instead.
+
+  Description:
+    None
+
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2020 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -20,19 +41,24 @@
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
 *******************************************************************************/
+// DOM-IGNORE-END
 
-#ifndef TOOLCHAIN_SPECIFICS_H
-#define TOOLCHAIN_SPECIFICS_H
+#ifndef ATSAMDA1J16B_COMPAT_H
 
+#ifndef DEVICE_DESC_BANK_NUMBER
+#define DEVICE_DESC_BANK_NUMBER USB_DEVICE_DESC_BANK_NUMBER
+#endif //DEVICE_DESC_BANK_NUMBER
 
-#include "cmsis_compiler.h"
-#include <sys/types.h>
-#define NO_INIT        __attribute__((section(".no_init")))
-#define SECTION(a)     __attribute__((__section__(a)))
+#ifndef HOST_DESC_BANK_NUMBER
+#define HOST_DESC_BANK_NUMBER USB_HOST_DESC_BANK_NUMBER
+#endif //HOST_DESC_BANK_NUMBER
 
-#define CACHE_LINE_SIZE    (4u)
-#define CACHE_ALIGN
+#ifndef DEVICE_ENDPOINT_NUMBER
+#define DEVICE_ENDPOINT_NUMBER USB_DEVICE_ENDPOINT_NUMBER
+#endif //DEVICE_ENDPOINT_NUMBER
 
+#ifndef HOST_PIPE_NUMBER
+#define HOST_PIPE_NUMBER USB_HOST_PIPE_NUMBER
+#endif //HOST_PIPE_NUMBER
 
-#endif // end of header
-
+#endif //ATSAMDA1J16B_COMPAT_H
