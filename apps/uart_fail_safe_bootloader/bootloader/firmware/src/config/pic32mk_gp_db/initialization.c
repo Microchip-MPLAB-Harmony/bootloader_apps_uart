21482140e0655f403c0828fc4c6b27611e68c685
Amit Raddi <amit.raddi@microchip.com>
2021-05-11 18:33:02 +0530

    [UART_FAIL_SAFE] Regenerated demo for uart_fail_safe_bootloader_bootloader_pic32mk_gp_db
    MH3-52406
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_fail_safe_bootloader/bootloader/firmware/src/config/pic32mk_gp_db/initialization.c
+++ apps/uart_fail_safe_bootloader/bootloader/firmware/src/config/pic32mk_gp_db/initialization.c
@@ -72,7 +72,7 @@
 /*** DEVCFG1 ***/
 #pragma config FNOSC =      SPLL
 #pragma config DMTINTV =    WIN_127_128
-#pragma config FSOSCEN =    ON
+#pragma config FSOSCEN =    OFF
 #pragma config IESO =       ON
 #pragma config POSCMOD =    OFF
 #pragma config OSCIOFNC =   OFF
@@ -176,7 +176,7 @@ void SYS_Initialize ( void* data )
 
     /* Configure Wait States and Prefetch */
     CHECONbits.PFMWS = 3;
-    CHECONbits.PREFEN = 1;
+    CHECONbits.PREFEN = 0;
 
 
 
