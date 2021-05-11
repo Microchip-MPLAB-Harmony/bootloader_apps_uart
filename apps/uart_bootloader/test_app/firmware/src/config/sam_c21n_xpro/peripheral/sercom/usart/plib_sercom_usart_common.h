9059fe688a28faaa5bdd5c610e3f9674c77885f6
Amit Raddi <amit.raddi@microchip.com>
2021-05-11 18:51:37 +0530

    [UART] Regenerated demo for uart_bootloader_test_app_sam_c21n_xpro
    MH3-52394
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/uart_bootloader/test_app/firmware/src/config/sam_c21n_xpro/peripheral/sercom/usart/plib_sercom_usart_common.h
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_c21n_xpro/peripheral/sercom/usart/plib_sercom_usart_common.h
@@ -66,6 +66,30 @@
 
 // *****************************************************************************
 // *****************************************************************************
+// Section:Preprocessor macros
+// *****************************************************************************
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* USART Error convenience macros */
+// *****************************************************************************
+// *****************************************************************************
+    /* Error status when no error has occurred */
+#define USART_ERROR_NONE 0U
+
+    /* Error status when parity error has occurred */
+#define USART_ERROR_PARITY SERCOM_USART_INT_STATUS_PERR_Msk
+
+    /* Error status when framing error has occurred */
+#define USART_ERROR_FRAMING SERCOM_USART_INT_STATUS_FERR_Msk
+
+    /* Error status when overrun error has occurred */
+#define USART_ERROR_OVERRUN SERCOM_USART_INT_STATUS_BUFOVF_Msk
+
+
+// *****************************************************************************
+// *****************************************************************************
 // Section: Data Types
 // *****************************************************************************
 // *****************************************************************************
@@ -85,21 +109,7 @@
     None.
 */
 
-typedef enum
-{
-    /* Error status when no error has occurred */
-    USART_ERROR_NONE,
-
-    /* Error status when parity error has occurred */
-    USART_ERROR_PARITY = SERCOM_USART_INT_STATUS_PERR_Msk,
-
-    /* Error status when framing error has occurred */
-    USART_ERROR_FRAMING = SERCOM_USART_INT_STATUS_FERR_Msk,
-
-    /* Error status when overrun error has occurred */
-    USART_ERROR_OVERRUN = SERCOM_USART_INT_STATUS_BUFOVF_Msk
-
-} USART_ERROR;
+typedef uint16_t USART_ERROR;
 
 // *****************************************************************************
 /* USART DATA
@@ -128,7 +138,7 @@ typedef enum
     USART_DATA_9_BIT = SERCOM_USART_INT_CTRLB_CHSIZE_9_BIT,
 
     /* Force the compiler to reserve 32-bit memory for each enum */
-    USART_DATA_INVALID = 0xFFFFFFFF
+    USART_DATA_INVALID = 0xFFFFFFFFU
 
 } USART_DATA;
 
@@ -158,7 +168,7 @@ typedef enum
     USART_PARITY_NONE = 0x2,
 
     /* Force the compiler to reserve 32-bit memory for each enum */
-    USART_PARITY_INVALID = 0xFFFFFFFF
+    USART_PARITY_INVALID = 0xFFFFFFFFU
 
 } USART_PARITY;
 
@@ -183,9 +193,32 @@ typedef enum
     USART_STOP_2_BIT = SERCOM_USART_INT_CTRLB_SBMODE_2_BIT,
 
     /* Force the compiler to reserve 32-bit memory for each enum */
-    USART_STOP_INVALID = 0xFFFFFFFF
+    USART_STOP_INVALID = 0xFFFFFFFFU
 
 } USART_STOP;
+
+// *****************************************************************************
+/* USART LIN Command
+
+  Summary:
+    Defines the data type for the USART peripheral LIN Command.
+
+  Description:
+    This may be used to set the USART LIN Master mode command.
+
+  Remarks:
+    None.
+*/
+
+typedef enum
+{
+    USART_LIN_MASTER_CMD_NONE = SERCOM_USART_INT_CTRLB_LINCMD_NONE,
+
+    USART_LIN_MASTER_CMD_SOFTWARE_CONTROLLED = SERCOM_USART_INT_CTRLB_LINCMD_SOFTWARE_CONTROL_TRANSMIT_CMD,
+
+    USART_LIN_MASTER_CMD_AUTO_TRANSMIT = SERCOM_USART_INT_CTRLB_LINCMD_AUTO_TRANSMIT_CMD
+
+} USART_LIN_MASTER_CMD;
 
 // *****************************************************************************
 /* USART Serial Configuration
@@ -270,6 +303,8 @@ typedef struct
 
     volatile bool                        rxBusyStatus;
 
+    volatile USART_ERROR                 errorStatus;
+
 } SERCOM_USART_OBJECT;
 
 
@@ -286,6 +321,9 @@ typedef enum
 
     /* Threshold number of free space is available in the transmit ring buffer */
     SERCOM_USART_EVENT_WRITE_THRESHOLD_REACHED,
+
+    /* Recevie break signal is detected */
+    SERCOM_USART_EVENT_BREAK_SIGNAL_DETECTED,
 }SERCOM_USART_EVENT;
 
 // *****************************************************************************
@@ -324,31 +362,37 @@ typedef struct
 {
     SERCOM_USART_RING_BUFFER_CALLBACK                   wrCallback;
 
-    uintptr_t                               			wrContext;
+    uintptr_t                                           wrContext;
 
-    volatile uint32_t                       			wrInIndex;
+    volatile uint32_t                                   wrInIndex;
 
-    volatile uint32_t                       			wrOutIndex;
+    volatile uint32_t                                   wrOutIndex;
 
-    bool                                    			isWrNotificationEnabled;
+    uint32_t                                            wrBufferSize;
 
-    uint32_t                                			wrThreshold;
+    bool                                                isWrNotificationEnabled;
 
-    bool                                    			isWrNotifyPersistently;
+    uint32_t                                            wrThreshold;
+
+    bool                                                isWrNotifyPersistently;
 
     SERCOM_USART_RING_BUFFER_CALLBACK                   rdCallback;
 
-    uintptr_t                               			rdContext;
+    uintptr_t                                           rdContext;
 
-    volatile uint32_t                       			rdInIndex;
+    volatile uint32_t                                   rdInIndex;
 
-    volatile uint32_t                       			rdOutIndex;
+    volatile uint32_t                                   rdOutIndex;
 
-    bool                                    			isRdNotificationEnabled;
+    uint32_t                                            rdBufferSize;
 
-    uint32_t                                			rdThreshold;
+    bool                                                isRdNotificationEnabled;
 
-    bool                                    			isRdNotifyPersistently;
+    uint32_t                                            rdThreshold;
+
+    bool                                                isRdNotifyPersistently;
+
+    volatile USART_ERROR                                errorStatus;
 
 } SERCOM_USART_RING_BUFFER_OBJECT;
 
