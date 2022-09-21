61f67581ce2db2e8b5347da7edb97fce16c20ad2
Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>
2022-09-21 08:56:55 +0530

    [UART] Regenerated demo for uart_bootloader_test_app_sam_c21n_xpro
    MH3-71318
    
    Signed-off-by: Sumanth Naga Venkata Ranga Suraneni <sumanth.suraneni@microchip.com>

--- apps/uart_bootloader/test_app/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
@@ -52,7 +52,6 @@
 // Section: PORT Implementation
 // *****************************************************************************
 // *****************************************************************************
-
 // *****************************************************************************
 /* Function:
 
@@ -75,20 +74,23 @@ void PORT_Initialize(void)
 
 
    /************************** GROUP 1 Initialization *************************/
-   PORT_REGS->GROUP[1].PORT_OUT = 0x80000;
-   PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x1;
-   PORT_REGS->GROUP[1].PORT_PINCFG[11] = 0x1;
-   PORT_REGS->GROUP[1].PORT_PINCFG[19] = 0x6;
+   PORT_REGS->GROUP[1].PORT_OUT = 0x80000U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x1U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[11] = 0x1U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[19] = 0x6U;
 
-   PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x33;
+   PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x33U;
+   PORT_REGS->GROUP[1].PORT_PMUX[9] = 0x0U;
 
    /************************** GROUP 2 Initialization *************************/
-   PORT_REGS->GROUP[2].PORT_DIR = 0x20;
-   PORT_REGS->GROUP[2].PORT_OUT = 0x20;
+   PORT_REGS->GROUP[2].PORT_DIR = 0x20U;
+   PORT_REGS->GROUP[2].PORT_OUT = 0x20U;
+   PORT_REGS->GROUP[2].PORT_PINCFG[5] = 0x0U;
+
+   PORT_REGS->GROUP[2].PORT_PMUX[2] = 0x0U;
 
 
 }
-
 // *****************************************************************************
 /* Function:
     uint32_t PORT_GroupRead(PORT_GROUP group)
@@ -298,7 +300,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
     PORT_GROUP group = GET_PORT_GROUP(pin);
     uint32_t pin_num = ((uint32_t)pin) & 0x1FU;
     uint32_t pinmux_val = (uint32_t)((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)];
-    
+
     /* For odd pins */
     if (0U != (pin_num & 0x01U))
     {
@@ -309,7 +311,7 @@ void PORT_PinPeripheralFunctionConfig(PORT_PIN pin, PERIPHERAL_FUNCTION function
         pinmux_val = (pinmux_val & ~0x0FU) | periph_func;
     }
     ((port_group_registers_t*)group)->PORT_PMUX[(pin_num >> 1)] = (uint8_t)pinmux_val;
-    
+
     /* Enable peripheral control of the pin */
     ((port_group_registers_t*)group)->PORT_PINCFG[pin_num] |= (uint8_t)PORT_PINCFG_PMUXEN_Msk;
 }
