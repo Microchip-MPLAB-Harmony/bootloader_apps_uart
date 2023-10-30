5754d0aee8341aabfd9453c933b40a3885b11249
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-10-30 18:28:01 +0530

    [APPS] Created mpu_uart_bootloader_bootloader_serial_flash_sam_a5d29_curiosity demo
    MH3-86441

---
+++ apps/mpu_uart_bootloader/bootloader_serial_flash/firmware/src/config/sam_a5d29_curiosity/bootloader/bootloader_storage.c
@@ -0,0 +1,253 @@
+/*******************************************************************************
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    bootloader_storage.c
+
+  Summary:
+    Storage for the Bootloader library.
+
+  Description:
+    This file contains the Storage definition for the Bootloader library.
+*******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2023 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Include Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <string.h>
+#include "configuration.h"
+#include "bootloader/bootloader_uart.h"
+
+typedef enum
+{
+    /* Transfer being processed */
+    BTL_MEM_TRANSFER_BUSY,
+
+    /* Transfer is successfully completed */
+    BTL_MEM_TRANSFER_COMPLETED,
+
+    /* Transfer had error*/
+    BTL_MEM_TRANSFER_ERROR_UNKNOWN
+
+} BTL_MEM_TRANSFER_STATUS;
+
+typedef struct
+{
+    bool deviceReady;
+
+    DRV_HANDLE handle;
+
+    /* External Memory image address */
+    uint32_t extMemoryImageAddr;
+
+    /* External Memory image size */
+    uint32_t extMemoryImageSize;
+
+    /* External Memory metadata address */
+    uint32_t extMemoryMetaDataAddr;
+
+    DRV_SST26_GEOMETRY geometry;
+
+    /* Programming address */
+    uint32_t progAddr;
+
+} BOOTLOADER_DATA;
+
+static uint8_t CACHE_ALIGN fileBuffer[PAGE_SIZE];
+static uint32_t memoryAddr;
+
+static BOOTLOADER_DATA btlData =
+{
+    .deviceReady = false,
+    .extMemoryImageAddr  = 0x00200000U,
+    .extMemoryMetaDataAddr = 0x00180000U,
+    .progAddr       = APP_START_ADDRESS,
+};
+
+static bool bootloader_WaitForXferComplete(void)
+{
+    bool status = false;
+
+    BTL_MEM_TRANSFER_STATUS transferStatus = BTL_MEM_TRANSFER_ERROR_UNKNOWN;
+
+    do
+    {
+        transferStatus = (BTL_MEM_TRANSFER_STATUS)DRV_SST26_TransferStatusGet(btlData.handle);
+
+    } while (transferStatus == BTL_MEM_TRANSFER_BUSY);
+
+    if(transferStatus == BTL_MEM_TRANSFER_COMPLETED)
+    {
+        status = true;
+    }
+
+    return status;
+}
+
+bool bootloader_IsDeviceReady(void)
+{
+    btlData.deviceReady = false;
+
+    if (DRV_SST26_Status(DRV_SST26_INDEX) == SYS_STATUS_READY)
+    {
+        btlData.handle = DRV_SST26_Open((SYS_MODULE_INDEX)DRV_SST26_INDEX, DRV_IO_INTENT_READWRITE);
+
+        if (btlData.handle != DRV_HANDLE_INVALID)
+        {
+            if (DRV_SST26_GeometryGet(btlData.handle, (void *)&btlData.geometry) == true)
+            {
+                btlData.deviceReady = true;
+            }
+        }
+    }
+    return btlData.deviceReady;
+}
+
+void bootloader_ImageSizeSet(uint32_t size)
+{
+    btlData.extMemoryImageSize = size;
+}
+
+void bootloader_Storage_Read(void)
+{
+    uint32_t readLen = 0U;
+    uint32_t imageSize;
+    bool status = false;
+
+    (void)DRV_SST26_Read(btlData.handle, fileBuffer, PAGE_SIZE, btlData.extMemoryMetaDataAddr);
+    (void)bootloader_WaitForXferComplete();
+
+    imageSize = *((uint32_t *)fileBuffer);
+
+    if (imageSize != 0xFFFFFFFFU)
+    {
+        memoryAddr = btlData.extMemoryImageAddr;
+
+        do
+        {
+            status = DRV_SST26_Read(btlData.handle, fileBuffer, PAGE_SIZE, memoryAddr);
+            (void)bootloader_WaitForXferComplete();
+            memcpy((void *)btlData.progAddr, fileBuffer, PAGE_SIZE);
+
+            btlData.progAddr += PAGE_SIZE;
+            readLen += PAGE_SIZE;
+            memoryAddr += PAGE_SIZE;
+
+        } while(readLen < imageSize);
+    }
+    if (status == true)
+    {
+        run_Application(APP_START_ADDRESS);
+    }
+}
+
+bool bootloader_Storage_Write(bool imageStartFlag, void *buffer, size_t size)
+{
+    bool status = false;
+
+    if (imageStartFlag)
+    {
+        memoryAddr = btlData.extMemoryImageAddr;
+    }
+
+    if ((memoryAddr % btlData.geometry.erase_blockSize) == 0U)
+    {
+        if (DRV_SST26_SectorErase(btlData.handle, memoryAddr) == true)
+        {
+            (void)bootloader_WaitForXferComplete();
+        }
+    }
+
+    status = DRV_SST26_PageWrite(btlData.handle, buffer, memoryAddr);
+    if (status == true)
+    {
+        (void)bootloader_WaitForXferComplete();
+    }
+
+    memoryAddr += PAGE_SIZE;
+
+    return status;
+}
+
+bool bootloader_Storage_CRC_Verify(uint32_t crc)
+{
+    uint32_t readLen = 0U;
+    uint32_t imageBufferSize;
+    uint32_t imageSize;
+    uint32_t crcGenerate = 0xFFFFFFFFU;
+    bool status = false;
+
+    memoryAddr = btlData.extMemoryImageAddr;
+
+    imageSize = btlData.extMemoryImageSize;
+    do
+    {
+        (void)DRV_SST26_Read(btlData.handle, fileBuffer, PAGE_SIZE, memoryAddr);
+        (void)bootloader_WaitForXferComplete();
+
+        if (imageSize >= PAGE_SIZE)
+        {
+            imageBufferSize = PAGE_SIZE;
+        }
+        else
+        {
+            imageBufferSize = imageSize;
+        }
+        crcGenerate = bootloader_CRCGenerate(fileBuffer, imageBufferSize, crcGenerate);
+
+        readLen += PAGE_SIZE;
+        memoryAddr += PAGE_SIZE;
+        imageSize -= imageBufferSize;
+
+        if ((imageSize == 0U) && (crc == crcGenerate))
+        {
+            status = true;
+            memoryAddr = btlData.extMemoryMetaDataAddr;
+
+            memcpy(fileBuffer, (uint8_t *)&btlData.extMemoryImageSize, sizeof(btlData.extMemoryImageSize));
+
+            if ((memoryAddr % btlData.geometry.erase_blockSize) == 0U)
+            {
+                if (DRV_SST26_SectorErase(btlData.handle, memoryAddr) == true)
+                {
+                    (void)bootloader_WaitForXferComplete();
+                }
+            }
+
+            (void)DRV_SST26_PageWrite(btlData.handle, fileBuffer, memoryAddr);
+            (void)bootloader_WaitForXferComplete();
+            break;
+        }
+    } while(readLen < btlData.extMemoryImageSize);
+
+    return status;
+}
---
+++ apps/mpu_uart_bootloader/bootloader_serial_flash/firmware/src/config/sam_a5d29_curiosity/initialization.c
@@ -0,0 +1,174 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
+// <editor-fold defaultstate="collapsed" desc="DRV_SST26 Initialization Data">
+
+static const DRV_SST26_PLIB_INTERFACE drvSST26PlibAPI = {
+    .CommandWrite   = QSPI1_CommandWrite,
+    .RegisterRead   = QSPI1_RegisterRead,
+    .RegisterWrite  = QSPI1_RegisterWrite,
+    .MemoryRead     = QSPI1_MemoryRead,
+    .MemoryWrite    = QSPI1_MemoryWrite
+};
+
+static const DRV_SST26_INIT drvSST26InitData =
+{
+    .sst26Plib      = &drvSST26PlibAPI,
+};
+// </editor-fold>
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local initialization functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* MISRAC 2012 deviation block end */
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+
+  
+    MMU_Initialize();
+    CLK_Initialize();
+    PIO_Initialize();
+
+
+
+    QSPI1_Initialize();
+
+	UART0_Initialize();
+
+    Matrix_Initialize();
+
+    AIC_INT_Initialize();
+    
+	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
+
+
+
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
+    sysObj.drvSST26 = DRV_SST26_Initialize((SYS_MODULE_INDEX)DRV_SST26_INDEX, (SYS_MODULE_INIT *)&drvSST26InitData);
+
+
+
+
+    /* MISRAC 2012 deviation block end */
+    APP_Initialize();
+
+
+
+
+    /* MISRAC 2012 deviation block end */
+}
+
+/*******************************************************************************
+ End of File
+*/
