68143616a07fdd1ca4d82b7f798ed1bd337385b7
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-12-06 20:12:47 +0530

    [UART] Regenerated demo for uart_bootloader_test_app_sam_l21_xpro
    MH3-88592

--- apps/uart_bootloader/test_app/firmware/src/config/sam_l21_xpro/definitions.h
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_l21_xpro/definitions.h
@@ -52,11 +52,11 @@
 #include "peripheral/sercom/usart/plib_sercom3_usart.h"
 #include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
-#include "peripheral/pm/plib_pm.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
+#include "peripheral/pm/plib_pm.h"
 #include "bsp/bsp.h"
 #include "app.h"
 
@@ -69,6 +69,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAML21J18B"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAML"
+#define DEVICE_SERIES		 "SAML21"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 48000000
