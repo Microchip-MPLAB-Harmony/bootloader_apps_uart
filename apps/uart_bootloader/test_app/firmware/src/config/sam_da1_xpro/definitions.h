8c6bb3053e47732350c1cfd95344a87e840c9522
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-12-06 20:07:57 +0530

    [UART] Regenerated demo for uart_bootloader_test_app_sam_da1_xpro
    MH3-88586

--- apps/uart_bootloader/test_app/firmware/src/config/sam_da1_xpro/definitions.h
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_da1_xpro/definitions.h
@@ -69,6 +69,12 @@ extern "C" {
 #endif
 // DOM-IGNORE-END
 
+/* Device Information */
+#define DEVICE_NAME			 "ATSAMDA1J16B"
+#define DEVICE_ARCH			 "CORTEX-M0PLUS"
+#define DEVICE_FAMILY		 "SAMD"
+#define DEVICE_SERIES		 "SAMDA1"
+
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 48000000
 
