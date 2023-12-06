3611c8f6eba395224a991d7b32a6209b1a964c72
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2023-12-06 20:05:03 +0530

    [UART] Regenerated demo for uart_bootloader_test_app_sam_c21n_xpro
    MH3-88582

--- apps/uart_bootloader/test_app/firmware/src/config/sam_c21n_xpro/bsp/bsp.h
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_c21n_xpro/bsp/bsp.h
@@ -40,8 +40,8 @@
 *******************************************************************************/
 // DOM-IGNORE-END
 
-#ifndef _BSP_H
-#define _BSP_H
+#ifndef BSP_H
+#define BSP_H
 
 // *****************************************************************************
 // *****************************************************************************
@@ -60,12 +60,15 @@
 // Section: BSP Macros
 // *****************************************************************************
 // *****************************************************************************
+#define sam_c21n_xpro
+#define BSP_NAME             "sam_c21n_xpro"
+
 
 
 /*** LED Macros for LED ***/
-#define LED_Toggle()     (PORT_REGS->GROUP[2].PORT_OUTTGL = 1 << 5)
-#define LED_On()         (PORT_REGS->GROUP[2].PORT_OUTCLR = 1 << 5)
-#define LED_Off()        (PORT_REGS->GROUP[2].PORT_OUTSET = 1 << 5)
+#define LED_Toggle()     (PORT_REGS->GROUP[2].PORT_OUTTGL = 1UL << 5)
+#define LED_On()         (PORT_REGS->GROUP[2].PORT_OUTCLR = 1UL << 5)
+#define LED_Off()        (PORT_REGS->GROUP[2].PORT_OUTSET = 1UL << 5)
 
 /*** SWITCH Macros for SWITCH ***/
 #define SWITCH_Get()     ((PORT_REGS->GROUP[1].PORT_IN >> 19) & 0x01)
@@ -105,7 +108,6 @@
 
   Example:
     <code>
-    //Initialize the BSP
     BSP_Initialize();
     </code>
 
@@ -115,7 +117,7 @@
 
 void BSP_Initialize(void);
 
-#endif // _BSP_H
+#endif // BSP_H
 
 /*******************************************************************************
  End of File
