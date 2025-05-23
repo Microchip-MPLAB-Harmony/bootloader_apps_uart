bea2d8516568f0f1129237a8a68129054ebfbf77
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-05-23 13:29:32 +0530

    [UART_BOOTLOADER] Regenerated demo for uart_bootloader__bootloader__firmware__sam_c21n_xpro.X
    MH3-113263

--- apps/uart_bootloader/bootloader/firmware/src/config/sam_c21n_xpro/initialization.c
+++ apps/uart_bootloader/bootloader/firmware/src/config/sam_c21n_xpro/initialization.c
@@ -127,7 +127,7 @@ void SYS_Initialize ( void* data )
 {
     /* MISRAC 2012 deviation block start */
     /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
-    NVMCTRL_Initialize();
+        NVMCTRL_Initialize();
 
 
     PORT_Initialize();
