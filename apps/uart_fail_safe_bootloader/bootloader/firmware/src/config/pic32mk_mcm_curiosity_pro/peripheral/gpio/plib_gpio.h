723e84e24b38ba42c109b1f64f353d9e266929e2
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-05-23 13:16:04 +0530

    [UART_FAIL_SAFE_BOOTLOADER] Regenerated demo for uart_fail_safe_bootloader__bootloader__firmware__pic32mk_mcm_curiosity_pro.X
    MH3-113375

--- apps/uart_fail_safe_bootloader/bootloader/firmware/src/config/pic32mk_mcm_curiosity_pro/peripheral/gpio/plib_gpio.h
+++ apps/uart_fail_safe_bootloader/bootloader/firmware/src/config/pic32mk_mcm_curiosity_pro/peripheral/gpio/plib_gpio.h
@@ -69,6 +69,7 @@
 #define SWITCH_OutputEnable()      (TRISGCLR = (1U<<11))
 #define SWITCH_InputEnable()       (TRISGSET = (1U<<11))
 #define SWITCH_Get()               ((PORTG >> 11) & 0x1U)
+#define SWITCH_GetLatch()          ((LATG >> 11) & 0x1U)
 #define SWITCH_PIN                  GPIO_PIN_RG11
 
 
@@ -243,7 +244,7 @@ void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
 
 static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
 {
-	 uint32_t xvalue = (uint32_t)value;
+     uint32_t xvalue = (uint32_t)value;
     GPIO_PortWrite((pin>>4U), (uint32_t)(0x1U) << (pin & 0xFU), (xvalue) << (pin & 0xFU));
 }
 
