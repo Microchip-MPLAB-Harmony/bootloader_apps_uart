26ae2cc3889966980830b78571b26529002bf20d
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-05-23 13:45:30 +0530

    [UART_BOOTLOADER] Regenerated demo for uart_bootloader__bootloader__firmware__sam_l22_xpro.X
    MH3-113285

--- apps/uart_bootloader/bootloader/firmware/src/config/sam_l22_xpro/definitions.h
+++ apps/uart_bootloader/bootloader/firmware/src/config/sam_l22_xpro/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -69,13 +69,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAML22N18A"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAML"
-#define DEVICE_SERIES		 "SAML22"
+#define DEVICE_NAME          "ATSAML22N18A"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAML"
+#define DEVICE_SERIES        "SAML22"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 32000000
+#define CPU_CLOCK_FREQUENCY 32000000U
 
 // *****************************************************************************
 // *****************************************************************************
