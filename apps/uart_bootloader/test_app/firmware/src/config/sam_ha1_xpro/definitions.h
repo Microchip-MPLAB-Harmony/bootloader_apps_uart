76ca1256c8519493fc6b6656b276aed270115eea
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-05-23 13:41:32 +0530

    [UART_BOOTLOADER] Regenerated demo for uart_bootloader__test_app__firmware__sam_ha1_xpro.X
    MH3-113347

--- apps/uart_bootloader/test_app/firmware/src/config/sam_ha1_xpro/definitions.h
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_ha1_xpro/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -70,13 +70,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMHA1G16AB"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAMH"
-#define DEVICE_SERIES		 "SAMHA1"
+#define DEVICE_NAME          "ATSAMHA1G16AB"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAMH"
+#define DEVICE_SERIES        "SAMHA1"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
