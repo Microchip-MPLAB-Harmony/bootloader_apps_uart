6f21231968318291cb7c35db7f074cc95540d94e
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-05-23 13:16:04 +0530

    [UART_BOOTLOADER] Regenerated demo for uart_bootloader__bootloader__firmware__pic32mk_mcm_curiosity_pro.X
    MH3-113247

--- apps/uart_bootloader/bootloader/firmware/src/config/pic32mk_mcm_curiosity_pro/initialization.c
+++ apps/uart_bootloader/bootloader/firmware/src/config/pic32mk_mcm_curiosity_pro/initialization.c
@@ -193,9 +193,9 @@ void SYS_Initialize ( void* data )
 
 
     CORETIMER_Initialize();
-    NVM_Initialize();
-
 	UART6_Initialize();
+
+    NVM_Initialize();
 
 
     /* MISRAC 2012 deviation block start */
