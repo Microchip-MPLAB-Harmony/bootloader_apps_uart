f930600e13cafbaca0221cad64d4b97caa0d1a88
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-05-23 13:22:30 +0530

    [UART_FAIL_SAFE_BOOTLOADER] Regenerated demo for uart_fail_safe_bootloader__bootloader__firmware__pic32mz_das_sk.X
    MH3-113377

--- apps/uart_fail_safe_bootloader/bootloader/firmware/src/config/pic32mz_das_sk/peripheral/gpio/plib_gpio.h
+++ apps/uart_fail_safe_bootloader/bootloader/firmware/src/config/pic32mz_das_sk/peripheral/gpio/plib_gpio.h
@@ -69,6 +69,7 @@
 #define SWITCH_OutputEnable()      (TRISBCLR = (1U<<13))
 #define SWITCH_InputEnable()       (TRISBSET = (1U<<13))
 #define SWITCH_Get()               ((PORTB >> 13) & 0x1U)
+#define SWITCH_GetLatch()          ((LATB >> 13) & 0x1U)
 #define SWITCH_PIN                  GPIO_PIN_RB13
 
 
@@ -288,7 +289,7 @@ void GPIO_PortOutputEnable(GPIO_PORT port, uint32_t mask);
 
 static inline void GPIO_PinWrite(GPIO_PIN pin, bool value)
 {
-	 uint32_t xvalue = (uint32_t)value;
+     uint32_t xvalue = (uint32_t)value;
     GPIO_PortWrite((pin>>4U), (uint32_t)(0x1U) << (pin & 0xFU), (xvalue) << (pin & 0xFU));
 }
 
