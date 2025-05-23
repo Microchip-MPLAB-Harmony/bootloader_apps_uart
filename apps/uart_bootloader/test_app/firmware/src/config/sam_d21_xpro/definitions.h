cda227371484400db0f0b7e7d7c60230a816b0b7
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-05-23 13:33:46 +0530

    [UART_BOOTLOADER] Regenerated demo for uart_bootloader__test_app__firmware__sam_d21_xpro.X
    MH3-113337

--- apps/uart_bootloader/test_app/firmware/src/config/sam_d21_xpro/definitions.h
+++ apps/uart_bootloader/test_app/firmware/src/config/sam_d21_xpro/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -70,13 +70,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "ATSAMD21J18A"
-#define DEVICE_ARCH			 "CORTEX-M0PLUS"
-#define DEVICE_FAMILY		 "SAMD"
-#define DEVICE_SERIES		 "SAMD21"
+#define DEVICE_NAME          "ATSAMD21J18A"
+#define DEVICE_ARCH          "CORTEX-M0PLUS"
+#define DEVICE_FAMILY        "SAMD"
+#define DEVICE_SERIES        "SAMD21"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 48000000
+#define CPU_CLOCK_FREQUENCY 48000000U
 
 // *****************************************************************************
 // *****************************************************************************
