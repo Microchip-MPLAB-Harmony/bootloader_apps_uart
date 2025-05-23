7d753d32d473d0709b4f80d1d4d63cfc309e139c
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-05-23 16:13:03 +0530

    [MPU_UART_BOOTLOADER] Regenerated demo for mpu_uart_bootloader__test_app__firmware__sam_a5d29_curiosity.X MH3-113225

---
+++ apps/mpu_uart_bootloader/test_app/firmware/src/packs/ATSAMA5D29_DFP/instance/piod.h
@@ -0,0 +1,39 @@
+/*
+ * Instance header file for ATSAMA5D27WLSOM1
+ *
+ * Copyright (c) 2024 Microchip Technology Inc. and its subsidiaries.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/* file generated from device description file (ATDF) version 2021-04-15T06:44:00Z */
+#ifndef _SAMA5D2_PIOD_INSTANCE_
+#define _SAMA5D2_PIOD_INSTANCE_
+
+
+/* ========== Instance Parameter definitions for PIOD peripheral ========== */
+#define PIOD_ABCDSR1_DEFAULT_VAL                 (0x00000000) /* Register value at reset */
+#define PIOD_ABCDSR2_DEFAULT_VAL                 (0x00000000) /* Register value at reset */
+#define PIOD_ABCDSR3_DEFAULT_VAL                 (0x00000000) /* Register value at reset */
+#define PIOD_ABCDSR4_DEFAULT_VAL                 (0x00000000) /* Register value at reset */
+#define PIOD_FILTRS_DEFAULT_VAL                  (0x00000000) /* Register value at reset */
+#define PIOD_INSTANCE_ID_D                       (70)       
+#define PIOD_OSR_DEFAULT_VAL                     (0x00000000) /* Register value at reset */
+#define PIOD_PPDSR_DEFAULT_VAL                   (0xFFFFFFFF) /* Register value at reset */
+#define PIOD_PSR_DEFAULT_VAL                     (0xFFFFFFFF) /* Register value at reset */
+#define PIOD_PUSR_DEFAULT_VAL                    (0x00000000) /* Register value at reset */
+#define PIOD_SECRS_DEFAULT_VAL                   (0x00000000) /* Register value at reset */
+#define PIOD_STRS_DEFAULT_VAL                    (0xFFFFFFFF) /* Register value at reset */
+
+#endif /* _SAMA5D2_PIOD_INSTANCE_ */
