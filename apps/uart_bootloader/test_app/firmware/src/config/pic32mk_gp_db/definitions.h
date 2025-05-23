a4aca2a5c47bce36c59b24151a7004adffa1e6ed
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-05-23 13:12:24 +0530

    [UART_BOOTLOADER] Regenerated demo for uart_bootloader__test_app__firmware__pic32mk_gp_db.X
    MH3-113311

--- apps/uart_bootloader/test_app/firmware/src/config/pic32mk_gp_db/definitions.h
+++ apps/uart_bootloader/test_app/firmware/src/config/pic32mk_gp_db/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -68,13 +68,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32MK1024GPE100"
-#define DEVICE_ARCH			 "MIPS"
-#define DEVICE_FAMILY		 "PIC32MK1402"
-#define DEVICE_SERIES		 "PIC32MK"
+#define DEVICE_NAME          "PIC32MK1024GPE100"
+#define DEVICE_ARCH          "MIPS"
+#define DEVICE_FAMILY        "PIC32MK1402"
+#define DEVICE_SERIES        "PIC32MK"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 120000000
+#define CPU_CLOCK_FREQUENCY 120000000U
 
 // *****************************************************************************
 // *****************************************************************************
--- apps/uart_bootloader/test_app/firmware/src/config/pic32mk_gp_db/peripheral/uart/plib_uart6.c
+++ apps/uart_bootloader/test_app/firmware/src/config/pic32mk_gp_db/peripheral/uart/plib_uart6.c
@@ -49,7 +49,7 @@
 // *****************************************************************************
 
 
-void static UART6_ErrorClear( void )
+static void UART6_ErrorClear( void )
 {
     UART_ERROR errors = UART_ERROR_NONE;
     uint8_t dummyData = 0u;
